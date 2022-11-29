c1de137ec413d5f31db9241b4c53732208063398
Aleks Ionin <starik.ionytch@gmail.com>
2022-11-29 23:42:19 +0200

    gallery added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const markup=galleryItems.map((item)=>` <div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div> `).join("");
+console.log (markup);
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+
+
+
+
+galleryContainer.addEventListener("click", selectImage);
+function selectImage(e) {
+    e.preventDefault();
+    console.log(e.target.nodeName);
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">`);
+
+    instance.show();
+    
+    
+};
