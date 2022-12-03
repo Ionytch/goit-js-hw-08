ae99418052fffff80701f9403df8726a9ec1b692
Aleks Ionin <starik.ionytch@gmail.com>
2022-12-03 17:21:20 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,12 @@
 var _ = require('lodash');
- import throttle from 'lodash'; 
    const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle(function (data) {
+player.on('timeupdate', _.throttle(function (data) {
     console.log(data.seconds);
     localStorage.setItem("videoplayer-current-time", data.seconds);
 },1000) );
             
-
-
 
 
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
