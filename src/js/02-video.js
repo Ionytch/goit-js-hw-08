7095988468719603f8cd18313c3e0c0c6aec1b61
Aleks Ionin <starik.ionytch@gmail.com>
2022-12-03 17:08:03 +0200

    task 2 added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+var _ = require('lodash');
+ import throttle from 'lodash'; 
+   const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', throttle(function (data) {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+},1000) );
+            
+
+
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
