b212c2a6657569e07227816af7bd4c250a9e79bf
Aleks Ionin <starik.ionytch@gmail.com>
2022-12-06 23:21:44 +0200

    files corrected

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
-var _ = require('lodash');
+import throttle from 'lodash.throttle';
 const feedbackForm = document.querySelector('.feedback-form');
 
-// let populateForm()
+
     const data = JSON.parse(localStorage.getItem('feedback-form-state'));
     const email = document.querySelector('.feedback-form input');
     const message = document.querySelector('.feedback-form textarea');
@@ -10,7 +10,7 @@ const feedbackForm = document.querySelector('.feedback-form');
         message.value = data.message;
     };
 
-const throtteledForm = _.throttle(() => {
+const throtteledForm = throttle(() => {
     
     const { email, message } = feedbackForm.elements;
     const back = { email: email.value, message: message.value };
@@ -30,33 +30,4 @@ feedbackForm.addEventListener('submit', event => {
     localStorage.removeItem('feedback-form-state');
 });
 
-// import throttle from 'lodash';
 
-// const form = document.querySelector('.feedback-form');
-// form.addEventListener('input', throttle(onFormData, 500));
-// form.addEventListener('submit', onSubmitForm);
-
-// const formData = {};
-
-// function onFormData(e) {
-//   formData[e.target.name] = e.target.value;
-//     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-//     console.log(e.target.value);
-// }
-
-// function onSubmitForm(e) {
-//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-//   e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem('feedback-form-state');
-// }
-
-// (function dataFromLocalStorage() {
-//   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-//   const email = document.querySelector('.feedback-form input');
-//   const message = document.querySelector('.feedback-form textarea');
-//   if (data) {
-//     email.value = data.email;
-//     message.value = data.message;
-//   }
-// });
